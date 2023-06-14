9a7ffa7ec4ff38c8cd3ddb73fe3d785a25189de0
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2023-06-14 13:03:15 -0400

    atualização/src/Category

--- src/components/CategoryCarousel/CategoryCarousel.js
+++ src/components/CategoryCarousel/CategoryCarousel.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, CarouselImg } from './style';
 import ImgCategory from '../../assets/categories.svg';
+import api from '../../services/api';
 
 function CategoryCarousel() {
+  useEffect(() => {
+    async function loadCategories() {
+      const response = await api.get('categories');
+
+      console.log(response);
+    }
+
+    loadCategories();
+  }, []);
+
   return (
     <Container>
       <CarouselImg src={ImgCategory} alt="Carousel-Home" />
